aa72efd8a725b06e1ac9a57e84d2f143413b78c8
T4T4el7 <ochoaarmando13@gmail.com>
2019-02-14 20:03:28 -0800

    set up initial band in town calls as well as the log.txt file and the secret keys

--- liri.js
+++ liri.js
@@ -1,3 +1,60 @@
+// Essential stuff
 require("dotenv").config();
-
+var axios = require("axios");
+var fs = require("fs");
 var keys = require("./keys.js");
+var Spotify = require("node-spotify-api")
+var spotify= new Spotify(keys.spotify);
+console.log(spotify);
+
+//Command line interactions
+var term= process.argv[2];
+console.log(term);
+var titulo= process.argv.slice(3).join(" ");
+console.log(titulo);
+
+// Action
+
+var start = function() {
+    if(term === "concert-this") {
+        show();}
+    // }else if( term === "spotify-this-song") {
+    //     music();
+    // }else if(term === "movie-this") {
+    //     movie();
+    // }
+    
+};
+
+var show = function() {
+    var artist = titulo;
+    console.log(artist);
+    var URL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    console.log(URL);
+    axios.get(URL).then(function(response) {
+        var jsonData = response.data;
+        var artistData = [
+            "Venue: " + jsonData[0].venue.name,
+            "Location: " + jsonData[0].venue.city,
+            "Date: " + jsonData[0].datetime
+        ].join("\n\n");
+
+        fs.appendFile("log.txt",artist + artistData, function(err) {
+            if(err) throw(err);
+            console.log(artistData);
+        });
+    });
+};
+
+
+// var music = function() {
+//     spotify.search({ type: "track",query: "All the small things"}, function(err, data) {
+//     if(err) {
+//         return console.log("Error occured " + err);
+//     }
+//     console.log(data);
+// });
+// };
+
+
+start();
